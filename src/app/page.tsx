7033533cd4e0d1505c8385d029a1018e879cc609
Andriy Petryna <andriypetrina2000@gmail.com>
2024-06-03 16:05:46 +0300

    fix: change size button on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,19 +12,19 @@ export default function Home() {
 				</h1>
 				<div className='flex flex-wrap justify-center  gap-5'>
 					<Link
-						className='p-2 w-32 text-center bg-blue-500 text-white rounded'
+						className='p-2 w-36 text-center bg-blue-500 text-white rounded'
 						href='/jobs'
 					>
 						Search Jobs
 					</Link>
 					<Link
-						className='p-2 w-32 text-center bg-green-500 text-white rounded'
+						className='p-2 w-36 text-center bg-green-500 text-white rounded'
 						href='/create-profile'
 					>
 						Create Profile
 					</Link>
 					<Link
-						className='p-2 w-32 text-center bg-red-500 text-white rounded'
+						className='p-2 w-36 text-center bg-red-500 text-white rounded'
 						href='/liked'
 					>
 						Liked Jobs
